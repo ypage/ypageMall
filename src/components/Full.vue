1ba913b9763650f582c29d708a353ddcc8ce3106
ypage <yy18770711861@163.com>
2018-10-09 15:19:52 +0800

    配置配置文件+引入vant vuex vue-router+项目架构

---
+++ src/components/Full.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Full',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
